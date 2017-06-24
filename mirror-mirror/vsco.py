5af83b1c85d1624ed3a2949fb062ed8f108c46f9
Matthew Nau <matthewnau@users.noreply.github.com>
2017-06-24 12:22:51 -0400

    updated vsco
    added error handling for accounts that have no pictures or don't exist

--- mirror-mirror/vsco.py
+++ mirror-mirror/vsco.py
@@ -10,35 +10,38 @@ def scrape_vsco(username):
 	#download the user's profile page
 	profile_page = s.get('http://vsco.co/'+username)
 
-	#get the unique session id from the site's cookies
-	unique_session_id = str(profile_page.cookies).split('vs=')[1]
-	unique_session_id = unique_session_id[:unique_session_id.index(' ')]
+	#check if account exists
+	if '<p class="page-error-heading mt40">This page does not exist</p>' not in profile_page.text:
 
-	#convert the profile page to a string
-	profile_page = profile_page.text
+		#get the unique session id from the site's cookies
+		unique_session_id = str(profile_page.cookies).split('vs=')[1]
+		unique_session_id = unique_session_id[:unique_session_id.index(' ')]
 
-	#get the user's unique user id from the profile page
-	unique_user_id = profile_page.split('"id":')[1]
-	unique_user_id = unique_user_id[:unique_user_id.index(',')]
+		#convert the profile page to a string
+		profile_page = profile_page.text
 
-	#find the user's profile picture link
-	profile_picture_link = profile_page.split('responsive_url":"')[1]
-	profile_picture_link = profile_picture_link[:profile_picture_link.index('"')]
-	
-	#add the profile picture link to the list
-	messy_links.append('http://'+profile_picture_link)
+		#get the user's unique user id from the profile page
+		unique_user_id = profile_page.split('"id":')[1]
+		unique_user_id = unique_user_id[:unique_user_id.index(',')]
 
-	#using the session and user id's, download the file containing the links to all pictures ever posted
-	user_links_page = s.get('http://vsco.co/ajxp/'+unique_session_id+'/2.0/medias?site_id='+unique_user_id+'&page=1&size=10000').text.split('"')
+		#find the user's profile picture link
+		profile_picture_link = profile_page.split('responsive_url":"')[1]
+		profile_picture_link = profile_picture_link[:profile_picture_link.index('"')]
+		
+		#add the profile picture link to the list
+		messy_links.append('http://'+profile_picture_link)
 
-	#collect the url of every possible jpg picture
-	for link in user_links_page:
-		if ((('im.vsco.co' in link) and ('.jpg' in link))):
-			messy_links.append('http://'+link)
+		#using the session and user id's, download the file containing the links to all pictures ever posted
+		user_links_page = s.get('http://vsco.co/ajxp/'+unique_session_id+'/2.0/medias?site_id='+unique_user_id+'&page=1&size=10000').text.split('"')
 
-	#find the uncompressed links to the images provided, and clean them up
-	for link in messy_links:
-		clean_links.append(link.replace('\\',''))
+		#collect the url of every possible jpg picture
+		for link in user_links_page:
+			if ((('im.vsco.co' in link) and ('.jpg' in link))):
+				messy_links.append('http://'+link)
+
+		#find the uncompressed links to the images provided, and clean them up
+		for link in messy_links:
+			clean_links.append(link.replace('\\',''))
 
 	#terminate the browsing session
 	s.close()
